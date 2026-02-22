f0d263b45c6b02de0e05f8f6a64bf95d59a2951b
infovishal01 <infovishalkumar01@gmail.com>
2026-02-22 23:26:52 +0530

    Reactjs

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div id = 'dad'>
+        <h1 id ='child1'>Hello</h1>
+        <h2 id='child2'>Hello bhai kaisa hai</h2>
+      </div>
+      <div id= "chacha"></div>
+    </>
+  )
+}
+
+export default App
+  
