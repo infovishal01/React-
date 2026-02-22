ebb7520d8564012bd10a1c7f67b9c4e4914d4516
infovishal01 <infovishalkumar01@gmail.com>
2026-02-22 23:39:35 +0530

    first commit

---
+++ 02-folder/src/component/Card.jsx
@@ -0,0 +1,9 @@
+function Card(){
+
+    return <div className='card'>
+            <h1>Vishal</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi dicta facilis non ipsa saepe obcaecati id illo quas! Minima animi iure molestiae, nemo expedita optio suscipit totam architecto repellat perspiciatis!</p>
+          </div>
+}
+
+export default Card;
